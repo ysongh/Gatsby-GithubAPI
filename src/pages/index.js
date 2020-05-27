025bb313e3185847090611b714d38e13f3bcf017
You Song Hou <yousonghou@yahoo.com>
2020-05-26 22:34:30 -0400

    set up gatsby app

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Gatsby and Github</h1>
+    <p>Find information about your organisation from Github</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
