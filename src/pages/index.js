b5f7d42a1b03c0891de3f41cc4e0d55efcbffb55
You Song Hou <yousonghou@yahoo.com>
2020-05-28 21:04:34 -0400

    add pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { TOKEN } from "../Token";
+import { TOKEN } from "../Token"
 
 class IndexPage extends Component{
   state = {
     data: [],
     organization: "",
-    error: ""
+    error: "",
+    page: 0,
+    total: 0
   }
 
-  async onSubmit(e){
-    e.preventDefault();
-
+  async onSubmit(page){
     const graphqlQuery = {
       query: `
         query { 
           organization(login: "${this.state.organization}"){
-            repositories(first: 12) {
+            repositories(first: ${(page + 1) * 12}) {
               totalCount
               nodes{
                 id
@@ -34,24 +33,37 @@ class IndexPage extends Component{
       `
     };
 
-    const result = await fetch('https://api.github.com/graphql', {
-      method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'bearer ' + TOKEN
-        },
-        body: JSON.stringify(graphqlQuery)
-    });
+    try{
+      const result = await fetch('https://api.github.com/graphql', {
+        method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'bearer ' + TOKEN
+          },
+          body: JSON.stringify(graphqlQuery)
+      });
+  
+      const resultData = await result.json()
 
-    const resultData = await result.json()
-    if(!resultData.data.organization){
-      this.setState({ error: "Not Found" });
+      if(!resultData.data.organization){
+        this.setState({ 
+          data: [],
+          error: "Not Found",
+          page: 0,
+          total: 0
+        })
+      }
+      else{
+        this.setState({ 
+          data: resultData.data.organization.repositories.nodes,
+          error: "",
+          page: page + 1,
+          total: resultData.data.organization.repositories.totalCount
+        })
+      }
     }
-    else{
-      this.setState({ 
-        data: resultData.data.organization.repositories.nodes,
-        error: ""
-     });
+    catch(err){
+      console.log(err)
     }
   }
 
@@ -62,28 +74,25 @@ class IndexPage extends Component{
   }
 
   render(){
-    console.log(this.state.data)
+    console.log(this.state.page, this.state.total)
     return(
       <Layout>
       <SEO title="Home" />
       <h1>Gatsby and Github</h1>
       <p>Find information about your organisation from Github</p>
-      <form className="form-inline" onSubmit={this.onSubmit.bind(this)}>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            name="organization"
-            placeholder="Organization Name"
-            value={this.state.organization}
-            onChange={this.onChange.bind(this)} 
-          />
-          <button type="submit" className="btn btn-primary">Search</button>
-        </div>
-        
-      </form>
+      <div className="form-group">
+        <input
+          className="form-control"
+          type="text"
+          name="organization"
+          placeholder="Organization Name"
+          value={this.state.organization}
+          onChange={this.onChange.bind(this)} 
+        />
+      </div>
+      <button className="btn btn-primary" onClick={this.onSubmit.bind(this, 0)}>Search</button>
 
-      <p className="text-danger">{this.state.error}</p>
+      <p className="mt-3 h4 text-center text-danger">{this.state.error}</p>
 
       <div className="row">
         {this.state.data.map(repo => {
@@ -101,6 +110,15 @@ class IndexPage extends Component{
           )
         })}
       </div>
+
+      {
+        this.state.page * 12 < this.state.total ? (
+          <button className="btn btn-primary d-block mx-auto" onClick={this.onSubmit.bind(this, this.state.page)}>
+            Load More
+          </button>
+        ) : null
+      }
+      
     </Layout>
     )
   }
